0baec99aee29a0e538079830e9f1cf6d3ea484f7
Zhiyang Fu <fuzhiyang0123@gmail.com>
2017-03-29 23:13:23 +0800

    init

---
+++ FCKClangPlugin/FCKClangPlugin/FCKClangPlugin.h
@@ -0,0 +1,13 @@
+//
+//  FCKClangPlugin.h
+//  FCKClangPlugin
+//
+//  Created by Zhiyang Fu on 29/03/2017.
+//  Copyright © 2017 Zhiyang Fu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FCKClangPlugin : NSObject
+
+@end
